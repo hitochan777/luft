a26cd90995eba99e2204b6336492e835d8112f67
Otsuki Hitoshi <hitochan777@gmail.com>
2018-06-12 00:22:01 +0900

    Add content in contact section

---
+++ src/components/Button.js
@@ -0,0 +1,24 @@
+import React from 'react'
+import injectSheet from 'react-jss'
+
+const styles = {
+  button: {
+    backgroundColor: 'white',
+    color: 'black',
+    textDecoration: 'none',
+    textAlign: 'center',
+    border: 'none',
+    width: '100%',
+    transition: '0.3s',
+    '&:hover': {
+      backgroundColor: 'black',
+      color: 'white',
+    },
+  },
+}
+
+const Button = ({ classes, children }) => (
+  <button className={classes.button}>{children}</button>
+)
+
+export default injectSheet(styles)(Button)
