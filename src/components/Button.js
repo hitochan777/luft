2f6fed4d53d07185a604d242adbc859c0beefe14
Otsuki Hitoshi <hitochan777@gmail.com>
2018-06-26 21:15:18 +0900

    Separate styling to scss file

--- src/components/Button.js
+++ src/components/Button.js
@@ -1,25 +1,9 @@
 import React from 'react'
-import injectSheet from 'react-jss'
 
-const styles = {
-  button: {
-    backgroundColor: 'white',
-    color: 'black',
-    textDecoration: 'none',
-    textAlign: 'center',
-    border: 'none',
-    width: '100%',
-    transition: '0.8s',
-    '&:hover': {
-      backgroundColor: 'black',
-      color: 'white',
-    },
-    fontSize: '0.7em',
-  },
-}
+import styles from './Button.module.scss'
 
-const Button = ({ classes, children }) => (
-  <button className={classes.button}>{children}</button>
+const Button = ({ children }) => (
+  <button className={styles.button}>{children}</button>
 )
 
-export default injectSheet(styles)(Button)
+export default Button
--- src/components/footer.js
+++ src/components/footer.js
@@ -1,25 +1,11 @@
 import React from 'react'
 import Link from './NavLink'
-import injectSheet from 'react-jss'
 import FontAwesome from 'react-fontawesome'
 
-const styles = {
-  footer: {
-    left: 0,
-    bottom: 0,
-  },
-  footer__nav: {
-    listStyleType: 'none',
-  },
-  footer__contact: {},
-  footer__copywriteBar: {
-    backgroundColor: 'black',
-    color: 'white',
-  },
-}
+import styles from './footer.module.scss'
 
-const Footer = ({ classes }) => (
-  <footer className={'container-fluid ' + classes.footer}>
+const Footer = () => (
+  <footer className={'container-fluid ' + styles.footer}>
     <div className={'row justify-content-center py-5'}>
       <div className={'col-2'}>
         <span>
@@ -34,17 +20,17 @@ const Footer = ({ classes }) => (
       </div>
       <div className={'col-2'}>
         <nav>
-          <ul className={classes.footer__nav}>
-            <li className={classes.nav__item}>
+          <ul className={styles.footer__nav}>
+            <li className={styles.nav__item}>
               <Link to="/concept"> > Concept</Link>
             </li>
-            <li className={classes.nav__item}>
+            <li className={styles.nav__item}>
               <Link to="/menu"> > Menu</Link>
             </li>
-            <li className={classes.nav__item}>
+            <li className={styles.nav__item}>
               <Link to="/info"> > Info</Link>
             </li>
-            <li className={classes.nav__item}>
+            <li className={styles.nav__item}>
               <Link to="/contact"> > Contact</Link>
             </li>
           </ul>
@@ -52,11 +38,11 @@ const Footer = ({ classes }) => (
       </div>
     </div>
     <div
-      className={'row justify-content-center ' + classes.footer__copywriteBar}
+      className={'row justify-content-center ' + styles.footer__copywriteBar}
     >
       <div className="col-1">©Luft</div>
     </div>
   </footer>
 )
 
-export default injectSheet(styles)(Footer)
+export default Footer
