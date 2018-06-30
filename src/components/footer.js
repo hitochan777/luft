7275b9cbdfc7e74a06dc279110ed3aa6ecfa1e11
Otsuki Hitoshi <hitochan777@gmail.com>
2018-06-30 18:59:15 +0900

    Improve footer design

--- src/components/footer.js
+++ src/components/footer.js
@@ -7,31 +7,31 @@ import styles from './footer.module.scss'
 const Footer = () => (
   <footer className={'container-fluid ' + styles.footer}>
     <div className={'row justify-content-center py-5'}>
-      <div className={'col-2'}>
+      <div className={'col-12 col-md-2'}>
         <span>
           <strong>Luft</strong>
         </span>
         <br />
-        <span>〒xxx-yyy　xx県yy市</span>
+        <span>〒xxx-yyy 兵庫県豊岡市若松町7-4</span>
         <br />
         <span>
           <FontAwesome name="phone" /> xxx-yyy-zzzz
         </span>
       </div>
-      <div className={'col-2'}>
-        <nav>
+      <div className={'col-12 col-md-2'}>
+        <nav className="row justify-content-center">
           <ul className={styles.footer__nav}>
             <li className={styles.nav__item}>
-              <Link to="/concept"> > Concept</Link>
+              <Link to="/concept">Concept</Link>
             </li>
             <li className={styles.nav__item}>
-              <Link to="/menu"> > Menu</Link>
+              <Link to="/stylist">Stylist</Link>
             </li>
             <li className={styles.nav__item}>
-              <Link to="/info"> > Info</Link>
+              <Link to="/menu">Menu</Link>
             </li>
             <li className={styles.nav__item}>
-              <Link to="/contact"> > Contact</Link>
+              <Link to="/info">Shop Information</Link>
             </li>
           </ul>
         </nav>
