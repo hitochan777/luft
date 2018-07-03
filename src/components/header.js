433f14d8a918492b8a74cfd9c9c100c4d8fe2b88
Otsuki Hitoshi <hitochan777@gmail.com>
2018-07-04 01:17:01 +0900

    Add custom navbar

--- src/components/header.js
+++ src/components/header.js
@@ -1,14 +1,6 @@
 import React from 'react'
-import {
-  Collapse,
-  Navbar,
-  NavbarToggler,
-  NavbarBrand,
-  Nav,
-  NavItem,
-  NavLink,
-} from 'reactstrap'
 
+import Navbar from './Navbar'
 import Link from './NavLink'
 import styles from './header.module.scss'
 import Logo from '../assets/img/logo.png'
@@ -30,43 +22,46 @@ export default class Header extends React.Component {
   render() {
     return (
       <div>
-        <Navbar
-          color="white"
-          light
-          expand="md"
-          className="py-0"
-          style={{ height: '60px' }}
-        >
-          <NavbarBrand tag={Link} to="/">
-            <img src={Logo} />
-          </NavbarBrand>
-          <NavbarToggler onClick={this.toggle} />
-          <Collapse isOpen={this.state.isOpen} navbar>
-            <Nav className="ml-auto" navbar>
-              <NavItem>
-                <NavLink tag={Link} to="/concept/">
-                  Concept
-                </NavLink>
-              </NavItem>
-              <NavItem>
-                <NavLink tag={Link} to="/stylist">
-                  Stylist
-                </NavLink>
-              </NavItem>
-              <NavItem>
-                <NavLink tag={Link} to="/menu">
-                  Menu
-                </NavLink>
-              </NavItem>
-              <NavItem>
-                <NavLink tag={Link} to="/info">
-                  Shop Information
-                </NavLink>
-              </NavItem>
-            </Nav>
-          </Collapse>
-        </Navbar>
+        <Navbar />
       </div>
     )
   }
 }
+
+// <Navbar
+//   color="white"
+//   light
+//   expand="md"
+//   className="py-0"
+//   style={{ height: '60px' }}
+// >
+//   <NavbarBrand tag={Link} to="/">
+//     <img src={Logo} />
+//   </NavbarBrand>
+//   <NavbarToggler onClick={this.toggle} />
+//   <Collapse isOpen={this.state.isOpen} navbar>
+//     <Nav className="ml-auto" navbar>
+//       <NavItem>
+//         <NavLink tag={Link} to="/concept/">
+//           Concept
+//         </NavLink>
+//       </NavItem>
+//       <NavItem>
+//         <NavLink tag={Link} to="/stylist">
+//           Stylist
+//         </NavLink>
+//       </NavItem>
+//       <NavItem>
+//         <NavLink tag={Link} to="/menu">
+//           Menu
+//         </NavLink>
+//       </NavItem>
+//       <NavItem>
+//         <NavLink tag={Link} to="/info">
+//           Shop Information
+//         </NavLink>
+//       </NavItem>
+//     </Nav>
+//   </Collapse>
+// </Navbar>
+//
