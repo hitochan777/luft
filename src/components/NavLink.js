46d836b59c423d086371e642076966d30fd80647
Otsuki Hitoshi <hitochan777@gmail.com>
2018-07-05 23:56:56 +0900

    Add NavLink

--- src/components/NavLink.js
+++ src/components/NavLink.js
@@ -1,8 +1,11 @@
 import React from 'react'
 import GatsbyLink from 'gatsby-link'
+import classNames from 'classnames'
 
 import styles from './NavLink.module.scss'
 
-const Link = ({ ...props }) => <GatsbyLink className={styles.link} {...props} />
+const Link = ({ className, ...props }) => (
+  <GatsbyLink className={classNames(styles.link, className)} {...props} />
+)
 
 export default Link
