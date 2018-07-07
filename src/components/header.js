d81a008fad207c733f9410e2f067f78a78e3489d
Otsuki Hitoshi <hitochan777@gmail.com>
2018-07-07 15:44:11 +0900

    Refactor

--- src/components/header.js
+++ src/components/header.js
@@ -1,9 +1,6 @@
 import React from 'react'
 
 import Navbar from './Navbar'
-import Link from './NavLink'
-import styles from './header.module.scss'
-import Logo from '../assets/img/logo.png'
 
 const Header = () => (
   <div>
