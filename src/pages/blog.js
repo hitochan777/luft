dd90315170092d93de3a83b86401075eaed6c0c0
Otsuki Hitoshi <hitochan777@gmail.com>
2018-08-05 17:03:26 +0900

    Show article depending on the path

--- src/pages/blog.js
+++ src/pages/blog.js
@@ -1,59 +1,30 @@
 import React from 'react'
 import { Route, Redirect } from 'react-router-dom'
-import { compose, lifecycle, withState } from 'recompose'
-import axios from 'axios'
+import BlogPosts from '../components/BlogPosts'
+import Blog from '../components/Blog'
 
-const withBlog = withState('blog', 'setBlog', {
-  title: '',
-  content: '',
-  date: '',
-  link: '',
-})
-
-const withPostId = withState('postId', 'setPostId', 1)
-
-const withLifecycle = lifecycle({
-  componentDidMount() {
-    // TODO: replace the endpoint with a real one
-    axios
-      .get(`https://demo.wp-api.org/wp-json/wp/v2/posts/${this.props.postId}`)
-      .then(post => {
-        const { data } = post
-        this.props.setBlog({
-          title: data.title.rendered,
-          content: data.content.rendered,
-          date: data.modified,
-          link: data.link,
-        })
-      })
-      .catch(err => {
-        console.log('Failed to fetch blog data for some reason')
-        console.log(err)
-      })
-  },
-})
-
-const getDateString = timestamp => {
-  const date = new Date(timestamp)
-  return `${date.getFullYear()}年${date.getMonth()}月${date.getDate()}日`
-}
-
-const Blog = ({ blog }) => (
+const withRouter = (BlogComponent, BlogListComponent) => () => (
   <div className="container-fluid">
-    <article className="row justify-content-center">
+    <div className="row justify-content-center">
       <div className="col-12 col-lg-6">
-        <div className="col-12">
-          <h1 href={blog.link}>{blog.title}</h1>
-        </div>
-        <div className="col-12">
-          <span>{getDateString(blog.date)}</span>
-        </div>
-        <div className="col-12">
-          <div dangerouslySetInnerHTML={{ __html: blog.content }} />
-        </div>
+        <Route
+          location={location}
+          key={location.key}
+          path="/blog/:postId"
+          component={({ match: { params } }) => (
+            <BlogComponent postId={params.postId} />
+          )}
+        />
+        <Route
+          location={location}
+          key={location.key}
+          exact
+          path="/blog"
+          component={({ match: { params } }) => <BlogListComponent />}
+        />
       </div>
-    </article>
+    </div>
   </div>
 )
 
-export default compose(withBlog, withPostId, withLifecycle)(Blog)
+export default withRouter(Blog, BlogPosts)
