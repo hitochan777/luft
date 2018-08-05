c6e6e61e4454beb29188abce0db3556672b39e63
Otsuki Hitoshi <hitochan777@gmail.com>
2018-08-05 15:44:11 +0900

    Enclose table body with tbody tag

--- src/components/BlogPosts.js
+++ src/components/BlogPosts.js
@@ -1,6 +1,7 @@
 import React from 'react'
 import { compose, lifecycle, withState } from 'recompose'
 import axios from 'axios'
+import Link from 'gatsby-link'
 
 const withBlogs = withState('blogs', 'setBlogs', [])
 
@@ -13,7 +14,6 @@ const withLifecycle = lifecycle({
       .get('https://demo.wp-api.org/wp-json/wp/v2/posts')
       .then(posts => {
         this.props.setBlogs(posts.data.slice(0, 5))
-        console.log(posts)
       })
       .catch(err => {
         console.log('Failed to fetch blog data for some reason')
@@ -27,19 +27,23 @@ const getDateString = timestamp => {
   return `${date.getFullYear()}年${date.getMonth()}月${date.getDate()}日`
 }
 
-const BlogUpdates = ({ blogs }) => (
+const BlogPosts = ({ blogs }) => (
   <table>
-    {blogs.map(blog => (
-      <tr key={blog.id}>
-        <td>
-          <span>{getDateString(blog.modified)}</span>
-        </td>
-        <td>
-          <a href={blog.link}>{extractInnerText(blog.title.rendered)}</a>
-        </td>
-      </tr>
-    ))}
+    <tbody>
+      {blogs.map(blog => (
+        <tr key={blog.id}>
+          <td>
+            <span>{getDateString(blog.modified)}</span>
+          </td>
+          <td>
+            <Link to={`/blog/${blog.id}`}>
+              {extractInnerText(blog.title.rendered)}
+            </Link>
+          </td>
+        </tr>
+      ))}
+    </tbody>
   </table>
 )
 
-export default compose(withBlogs, withLifecycle)(BlogUpdates)
+export default compose(withBlogs, withLifecycle)(BlogPosts)
