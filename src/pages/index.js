bdb7fff494eb1ac189084c333e1d3fd5867ab90d
Otsuki Hitoshi <hitochan777@gmail.com>
2018-08-12 22:01:03 +0900

    Stop using inline styling for google map

--- src/pages/index.js
+++ src/pages/index.js
@@ -111,18 +111,15 @@ const IndexPage = () => (
             </div>
           </div>
         </div>
-        <div className="row justify-content-center">
-          <iframe
-            src={`https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3246.186222723647!2d134.81301481571583!3d35.54910278022534!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x5fffc43e1396ea11%3A0x535fd767914f80fe!2z772I772B772J772S77ys772V772G772U!5e0!3m2!1sen!2sjp!4v1527418838679`}
-            width="100%"
-            height="450"
-            frameBorder="0"
-            style={{ border: '0' }}
-            className="col-12"
-            allowFullScreen
-          />
-        </div>
       </section>
+    </ScrollAnimation>
+    <ScrollAnimation animateIn="fadeIn" duration={2} animateOnce={true}>
+      <iframe
+        src={`https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3246.186222723647!2d134.81301481571583!3d35.54910278022534!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x5fffc43e1396ea11%3A0x535fd767914f80fe!2z772I772B772J772S77ys772V772G772U!5e0!3m2!1sen!2sjp!4v1527418838679`}
+        frameBorder="0"
+        className={styles.map}
+        allowFullScreen
+      />
     </ScrollAnimation>
   </div>
 )
