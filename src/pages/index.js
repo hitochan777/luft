b5ae80e72766eca998a174f3a308542cd8c31688
Otsuki Hitoshi <hitochan777@gmail.com>
2018-09-08 20:46:21 +0900

    Add NavForSmallDevice component

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,11 +1,26 @@
 import React from 'react'
-import ScrollAnimation from 'react-animate-on-scroll'
 import Link from 'gatsby-link'
+import styled from 'styled-components'
 
 import Jumbotron from '../components/Jumbotron'
 
-import styles from './index.module.scss'
+const NavForSmallDevice = styled.div`
+  @media screen and (min-width: ${props => props.theme.threshold}) {
+    display: none;
+  }
+`
 
-const IndexPage = () => <Jumbotron />
+const IndexPage = () => (
+  <div>
+    <Jumbotron />
+    <NavForSmallDevice>
+      <Link to="/concept">Concept</Link>
+      <Link to="/stylist">Stylists</Link>
+      <Link to="/info">Shop Information</Link>
+      <Link to="/menu">Menu</Link>
+      <Link to="/news">News</Link>
+    </NavForSmallDevice>
+  </div>
+)
 
 export default IndexPage
