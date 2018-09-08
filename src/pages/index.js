edd1fa55362492c937731d0456fd0d73a1897582
Otsuki Hitoshi <hitochan777@gmail.com>
2018-09-08 22:55:25 +0900

    Add columized menu that appears only on mobile

--- src/pages/index.js
+++ src/pages/index.js
@@ -5,20 +5,29 @@ import styled from 'styled-components'
 import Jumbotron from '../components/Jumbotron'
 
 const NavForSmallDevice = styled.div`
+  display: flex;
+  flex-direction: column;
+  margin-top: -20px;
   @media screen and (min-width: ${props => props.theme.threshold}) {
     display: none;
   }
+`
+
+const StyledLink = styled(Link)`
+  text-align: center;
+  text-decoration: none;
+  color: ${props => props.theme.luftColor};
 `
 
 const IndexPage = () => (
   <div>
     <Jumbotron />
     <NavForSmallDevice>
-      <Link to="/concept">Concept</Link>
-      <Link to="/stylist">Stylists</Link>
-      <Link to="/info">Shop Information</Link>
-      <Link to="/menu">Menu</Link>
-      <Link to="/news">News</Link>
+      <StyledLink to="/concept">Concept</StyledLink>
+      <StyledLink to="/stylist">Stylists</StyledLink>
+      <StyledLink to="/info">Shop Information</StyledLink>
+      <StyledLink to="/menu">Menu</StyledLink>
+      <StyledLink to="/news">News</StyledLink>
     </NavForSmallDevice>
   </div>
 )
