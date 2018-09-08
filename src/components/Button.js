38fc2063d849c994f1c2c33c857deebbd3c229a4
Otsuki Hitoshi <hitochan777@gmail.com>
2018-09-08 20:49:00 +0900

    Use styled-components for button

--- src/components/Button.js
+++ src/components/Button.js
@@ -1,9 +1,18 @@
 import React from 'react'
+import styled from 'styled-components'
 
-import styles from './Button.module.scss'
-
-const Button = ({ children }) => (
-  <button className={styles.button}>{children}</button>
-)
+const Button = styled.button`
+  background-color: white;
+  color: black;
+  text-decoration: none;
+  text-align: center;
+  border: none;
+  width: 100%;
+  transition: 0.8s;
+  &:hover {
+    background-color: black;
+    color: white;
+  }
+`
 
 export default Button
