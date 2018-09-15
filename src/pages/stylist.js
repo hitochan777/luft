4c1990038e5fc4fd2ef374e095dd7f60f2b5bda1
Otsuki Hitoshi <hitochan777@gmail.com>
2018-09-15 23:30:29 +0900

    Change style of stylist page

--- src/pages/stylist.js
+++ src/pages/stylist.js
@@ -13,55 +13,53 @@ const StylistDescription = styled.div`
 
 const StylistName = ({ name, experienceYears }) => (
   <span>
-    {name} (美容師歴{experienceYears}年 )
+    <h2>{name}</h2> (美容師歴{experienceYears}年 )
   </span>
 )
 
-const Stylist = () => (
+const Stylist = styled.div``
+
+const Stylists = () => (
   <Content title="Stylist">
     <Container>
       <Row>
-        <Col>
-          <StylistDescription>
-            Stylist①<br />
-            <StylistName name="ATSUSHI" experienceYears={25} />
-            <StylistDescItem title="趣味">スノボー、ゴルフ</StylistDescItem>
-            <StylistDescItem title="好きな言葉">
-              なんとか、なるって
-            </StylistDescItem>
-            <StylistDescItem title="好きな食べ物">焼肉</StylistDescItem>
-          </StylistDescription>
+        <Col sm={6} xs={12}>
+          <Stylist>
+            <img
+              src={Image1}
+              height="200px"
+              width="100%"
+              style={{ objectFit: 'cover' }}
+            />
+            <StylistDescription>
+              <StylistName name="ATSUSHI" experienceYears={25} />
+              <StylistDescItem title="趣味">スノボー、ゴルフ</StylistDescItem>
+              <StylistDescItem title="好きな言葉">
+                なんとか、なるって
+              </StylistDescItem>
+              <StylistDescItem title="好きな食べ物">焼肉</StylistDescItem>
+            </StylistDescription>
+          </Stylist>
         </Col>
-        <Col>
-          <img
-            src={Image1}
-            height="200px"
-            width="100%"
-            style={{ objectFit: 'cover' }}
-          />
-        </Col>
-      </Row>
-      <Row>
-        <Col>
-          <StylistDescription>
-            Stylist②<br />
-            <StylistName name="YUKA" experienceYears={18} />
-            <StylistDescItem title="趣味">スノボー、ゴルフ</StylistDescItem>
-            <StylistDescItem title="好きな言葉">一期一会</StylistDescItem>
-            <StylistDescItem title="好きな食べ物">パンケーキ</StylistDescItem>
-          </StylistDescription>
-        </Col>
-        <Col>
-          <img
-            src={Image1}
-            height="200px"
-            width="100%"
-            style={{ objectFit: 'cover' }}
-          />
+        <Col sm={6} xs={12}>
+          <Stylist>
+            <img
+              src={Image1}
+              height="200px"
+              width="100%"
+              style={{ objectFit: 'cover' }}
+            />
+            <StylistDescription>
+              <StylistName name="YUKA" experienceYears={18} />
+              <StylistDescItem title="趣味">スノボー、ゴルフ</StylistDescItem>
+              <StylistDescItem title="好きな言葉">一期一会</StylistDescItem>
+              <StylistDescItem title="好きな食べ物">パンケーキ</StylistDescItem>
+            </StylistDescription>
+          </Stylist>
         </Col>
       </Row>
     </Container>
   </Content>
 )
 
-export default Stylist
+export default Stylists
