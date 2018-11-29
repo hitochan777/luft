1e48e75e2682986c2f2eba759d0799c2f46a8413
Otsuki Hitoshi <hitochan777@gmail.com>
2018-11-30 02:44:36 +0900

    Use contentful for blog post list

--- src/pages/news.js
+++ src/pages/news.js
@@ -1,10 +1,42 @@
 import React from 'react'
+import Link from 'gatsby-link'
+import { graphql } from 'gatsby'
 
 import Content from '../components/SubPage'
 import Layout from '../components/Layout'
 
-export default () => (
-  <Layout>
-    <Content title="News">hoge</Content>
-  </Layout>
-)
+export default ({ data }) => {
+  const blogs = data.allContentfulBlogPost.edges.map(edge => edge.node)
+  return (
+    <Layout>
+      <Content title="News">
+        {blogs.map(blog => (
+          <article key={blog.id}>
+            <h2>
+              <Link to={`/blog/${blog.id}`}>{blog.title}</Link>
+            </h2>
+            {blog.publishDate} <br />
+            {blog.description.description}
+          </article>
+        ))}
+      </Content>
+    </Layout>
+  )
+}
+
+export const pageQuery = graphql`
+  {
+    allContentfulBlogPost {
+      edges {
+        node {
+          id
+          publishDate(formatString: "YYYY月MM月DD日", locale: "ja-JP")
+          description {
+            description
+          }
+          title
+        }
+      }
+    }
+  }
+`
