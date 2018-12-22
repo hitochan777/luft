ccf39bf91978265a9bc539acb07d9a863792a23a
Otsuki Hitoshi <hitochan777@gmail.com>
2018-12-23 03:06:13 +0900

    Change font-style of title in news item to normal

--- src/components/molecule/NewsItem.jsx
+++ src/components/molecule/NewsItem.jsx
@@ -1,36 +1,32 @@
-import React from "react"
-import styled from "styled-components"
+import React from 'react'
+import styled from 'styled-components'
 
 import Link from '../atom/Link'
-import Image from "../atom/Image"
+import Image from '../atom/Image'
 
 const Thumbnail = styled(Image)`
-    border-radius: 10px;
+  border-radius: 10px;
 `
 
 const StyledNewsItem = styled.article`
-    display: flex;
-    flex-direction: column;
-    word-wrap: break-word;
-    @media screen and (min-width: ${props => props.theme.threshold}) {
-      min-height: 400px;
-      height: 400px;
-    }
-    position: relative;
+  display: flex;
+  flex-direction: column;
+  word-wrap: break-word;
+  @media screen and (min-width: ${props => props.theme.threshold}) {
+    min-height: 400px;
+    height: 400px;
+  }
+  position: relative;
   @media screen and (max-width: ${props => props.theme.threshold}) {
     margin-top: 30px;
   }
-
 `
 
 const Title = styled.h2`
-    font-weight: bold;
-    margin-top: 10px;
+  margin-top: 10px;
 `
 
-const Description = styled.p`
-`
-
+const Description = styled.p``
 
 const ReadMoreButton = styled.button`
   background-color: ${props => props.theme.luftColor};
@@ -39,27 +35,30 @@ const ReadMoreButton = styled.button`
   height: 40px;
   width: 100%;
   border-radius: 10px;
-  transition: all .2s ease-in-out;
+  transition: all 0.2s ease-in-out;
   &:hover {
-      background-color: white;
-      color: ${props => props.theme.luftColor};
+    background-color: white;
+    color: ${props => props.theme.luftColor};
   }
 `
 
 const ButtonLink = styled(Link)`
-
   @media screen and (min-width: ${props => props.theme.threshold}) {
-  position: absolute;
-  bottom: 0;
+    position: absolute;
+    bottom: 0;
   }
   width: 100%;
 `
 
-const NewsItem = ({blogId, imagePath, title, description, publishDate}) => <StyledNewsItem>
-    <Thumbnail src={imagePath}/>
+const NewsItem = ({ blogId, imagePath, title, description, publishDate }) => (
+  <StyledNewsItem>
+    <Thumbnail src={imagePath} />
     <Title>{title}</Title>
     <Description>{description}</Description>
-    <ButtonLink to={`/blog/${blogId}`} hasUnderline={false}><ReadMoreButton>もっと読む</ReadMoreButton></ButtonLink>
-</StyledNewsItem>
+    <ButtonLink to={`/blog/${blogId}`} hasUnderline={false}>
+      <ReadMoreButton>もっと読む</ReadMoreButton>
+    </ButtonLink>
+  </StyledNewsItem>
+)
 
 export default NewsItem
