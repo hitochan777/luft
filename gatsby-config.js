72359c6f5e9620a3d754d5105cc07f497ffa5a65
Otsuki Hitoshi <hitochan777@gmail.com>
2018-12-23 04:12:08 +0900

    Change title

--- gatsby-config.js
+++ gatsby-config.js
@@ -4,7 +4,7 @@ require('dotenv').config({
 
 module.exports = {
   siteMetadata: {
-    title: 'Gatsby Default Starter',
+    title: 'hairLuft【ヘアールフト】',
   },
   plugins: [
     'gatsby-plugin-react-helmet',
