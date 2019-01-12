e02c81ec294ad87617b77dc1bf3ed760a12b1c2e
Otsuki Hitoshi <hitochan777@gmail.com>
2019-01-12 13:34:40 +0900

    Add animation to cover image

--- src/components/atom/Image.jsx
+++ src/components/atom/Image.jsx
@@ -1,5 +1,24 @@
 import React from 'react'
+import styled from 'styled-components'
 
 const Image = ({ src, alt, ...props }) => <img src={src} alt={alt} {...props} />
+
+export const CoverImage = styled(Image)`
+  @keyframes cover-image {
+    from {
+      opacity: 0;
+      transform: scale(1.05);
+    }
+    to {
+      opacity: 1;
+      transform: scale(1);
+    }
+  }
+  animation-name: cover-image;
+  animation-duration: 5s;
+  object-fit: cover;
+  height: 400px;
+  width: 100%;
+`
 
 export default Image
