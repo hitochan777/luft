ba66549982114c1522cac49f77000b2cbad85357
Otsuki Hitoshi <hitochan777@gmail.com>
2019-01-12 19:10:39 +0900

    Change to hamburger menu

--- src/components/molecule/Navbar/index.jsx
+++ src/components/molecule/Navbar/index.jsx
@@ -6,34 +6,26 @@ import Logo from '../../../assets/img/logo.png'
 import NavbarToggleButton from './NavbarToggleButton'
 import Link from '../../atom/Link'
 
-const StyledNavbarSide = styled.div`
-  width: 100%;
-  height: 80px;
-  @media screen and (min-width: ${props => props.theme.tablet_width}) {
-    position: fixed;
-    left: 0;
-    top: 0;
-    display: flex;
-    flex-direction: column;
-    background-color: white;
-    height: calc(100vh - ${props => props.theme.footerHeight});
-    width: ${props => props.theme.sideNavbarWidth};
-    min-width: ${props => props.theme.sideNavbarWidth};
-    justify-content: center;
-    z-index: 100;
-  }
-`
-
 const NavbarBrand = styled.img`
-  margin-left: 10px;
+  margin: 0 auto;
   display: block;
   @media screen and (max-width: ${props => props.theme.tablet_width}) {
     padding-top: 14px;
     padding-bottom: 14px;
   }
 `
-const NavbarMain = styled.ul`
-  list-style: none;
+const Navbar = styled.div`
+  display: flex;
+  flex-direction: column;
+  position: fixed;
+  background-color: #f7f7f7;
+  margin: 0;
+  padding: 0;
+  padding-top: 15vh;
+  left: 0;
+  top: 0;
+  height: 100vh;
+  z-index: 150;
 
   @keyframes fadein {
     from {
@@ -44,71 +36,116 @@ const NavbarMain = styled.ul`
     }
   }
 
+  @media screen and (min-width: ${props => props.theme.tablet_width}) {
+    width: ${props => props.theme.sideNavbarWidth};
+    min-width: ${props => props.theme.sideNavbarWidth};
+  }
+
   @media screen and (max-width: ${props => props.theme.tablet_width}) {
     animation: fadein 0.5s;
     display: ${props => (props.isOpen ? 'flex' : 'none')};
-    flex-direction: column;
-    justify-content: center;
-    text-align: center;
-    position: fixed;
-    width: 100vw;
-    height: 100vh;
-    z-index: 150;
-    top: 0;
-    left: 0;
+    width: 215px;
     bottom: 0;
     right: 0;
     margin: 0;
-    padding: 0;
-    background-color: white;
   }
 `
 const NavItem = styled.li`
+  margin: 0 auto;
+  color: ${props => props.theme.greyColor};
+  padding-bottom: 5px;
+  padding-bottom: 5px;
   position: relative;
-  margin-left: 20px;
-  margin-top: 10px;
-  margin-bottom: 10px;
-  @media screen and (max-width: ${props => props.theme.tablet_width}) {
-    margin-left: 0;
+  text-align: center;
+  text-transform: uppercase;
+  &:hover {
+    background-color: white;
   }
 `
 
-const NavbarSide = ({ isNavbarOpen, setIsNavbarOpen }) => {
-  const NavLinkWithCloseAction = props => (
+const Header = styled.div``
+
+const Main = styled.ul`
+  list-style: none;
+  margin: 0;
+  margin-top: 20px;
+`
+
+const Footer = styled.div`
+  position: absolute;
+  text-align: center;
+  width: 100%;
+  bottom: 30px;
+`
+
+const withNavbarOpen = withState('isNavbarOpen', 'setIsNavbarOpen', false)
+
+const NavLink = withState('isNavbarOpen', 'setIsNavbarOpen', false)(
+  ({ isNavbarOpen, setIsNavbarOpen, ...props }) => (
     <NavItem
       onClick={() => {
         setIsNavbarOpen(false)
       }}
     >
-      <Link {...props} />
+      <Link
+        {...props}
+        style={{ margin: '0 35px', borderBottom: '1px solid #e2e2e2' }}
+      />
     </NavItem>
   )
+)
+
+const StyledNavCloseCover = styled.span`
+  @media screen and (max-width: ${props => props.theme.tablet_width}) {
+    @keyframes fadein {
+      from {
+        opacity: 0;
+      }
+      to {
+        opacity: 1;
+      }
+    }
+    top: 0;
+    left: 0;
+    position: fixed;
+    background-color: rgba(0, 0, 0, 0.6);
+    width: 100vw;
+    height: 100vh;
+    z-index: 125;
+    animation: fadein 0.5s;
+  }
+`
+const NavCloseCover = ({ setIsNavbarOpen }) => (
+  <StyledNavCloseCover onClick={() => setIsNavbarOpen(false)} />
+)
+
+const NavbarSide = ({ isNavbarOpen, setIsNavbarOpen }) => {
   return (
-    <StyledNavbarSide>
+    <>
       <NavbarToggleButton
         onClick={() => {
           setIsNavbarOpen(!isNavbarOpen)
         }}
         isCollapsed={!isNavbarOpen}
       />
-      <Link to="/" hasUnderline={false}>
-        <NavbarBrand src={Logo} />
-      </Link>
-      <NavbarMain isOpen={isNavbarOpen}>
-        <NavLinkWithCloseAction to="/concept">Concept</NavLinkWithCloseAction>
-        <NavLinkWithCloseAction to="/info#stylists">
-          Stylists
-        </NavLinkWithCloseAction>
-        <NavLinkWithCloseAction to="/info#access">
-          Access
-        </NavLinkWithCloseAction>
-        <NavLinkWithCloseAction to="/menu">Menu</NavLinkWithCloseAction>
-        <NavLinkWithCloseAction to="/news">News</NavLinkWithCloseAction>
-      </NavbarMain>
-    </StyledNavbarSide>
+      {isNavbarOpen && <NavCloseCover setIsNavbarOpen={setIsNavbarOpen} />}
+      <Navbar isOpen={isNavbarOpen}>
+        <Header>
+          <Link to="/" hasUnderline={false}>
+            <NavbarBrand src={Logo} />
+          </Link>
+        </Header>
+        <Main>
+          <NavLink to="/concept">Concept</NavLink>
+          <NavLink to="/info#stylists">Stylists</NavLink>
+          <NavLink to="/info#access">Access</NavLink>
+          <NavLink to="/menu">Menu</NavLink>
+          <NavLink to="/news">News</NavLink>
+        </Main>
+        <Footer>©hair Luft</Footer>
+      </Navbar>
+    </>
   )
 }
 
-export default compose(withState('isNavbarOpen', 'setIsNavbarOpen', false))(
-  NavbarSide
-)
+export default compose(withNavbarOpen)(NavbarSide)
