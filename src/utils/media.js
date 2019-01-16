3016f1e5187b220797a4028e26fe1372af7d0498
Otsuki Hitoshi <hitochan777@gmail.com>
2019-01-16 23:49:53 +0900

    Add media helper function

---
+++ src/utils/media.js
@@ -0,0 +1,10 @@
+import sizes from '../theme'
+
+export default Object.keys(sizes).reduce((acc, label) => {
+  acc[label] = (...args) => css`
+    @media (max-width: ${sizes[label] / 16}em) {
+      ${css(...args)}
+    }
+  `
+  return acc
+}, {})
