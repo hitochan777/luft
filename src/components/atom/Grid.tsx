b8b461d9fb2d4e18c0bf52533ba41cf465cbb7b5
Otsuki Hitoshi <hitochan777@gmail.com>
2019-03-09 00:02:12 +0900

    Add type to Grid.tsx

--- src/components/atom/Grid.tsx
+++ src/components/atom/Grid.tsx
@@ -1,5 +1,10 @@
 import React from 'react'
-import { Flex as GridFlex, Box as GridBox } from '@rebass/grid'
+import {
+  Flex as GridFlex,
+  Box as GridBox,
+  FlexProps,
+  BoxProps,
+} from '@rebass/grid'
 
-export const Flex = props => <GridFlex {...props} my="2rem" />
-export const Box = props => <GridBox {...props} my="2rem" />
+export const Flex = (props: FlexProps) => <GridFlex {...props} my="2rem" />
+export const Box = (props: BoxProps) => <GridBox {...props} my="2rem" />
